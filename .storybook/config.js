89f4b6c0466abd1dc92abdd05e3c58095a8536ba
armen.melkonyan <armen.melkonyan@sfl.am>
2019-02-28 14:15:41 +0400

    BANK-32: Implement checkbox

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,8 @@ import { configure } from '@storybook/react';
 
 function loadStories() {
   require('../src/components/button/button.story.js');
+  require('../src/components/Checkbox/Checkbox.story.js');
+  require('../src/components/checkboxGroup/CheckboxGroup.story.js');
 }
 
 configure(loadStories, module);
--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  plugins: {
-    'postcss-preset-env': {
-      importFrom: 'src/defaults.css',
-      features: {
-        'nesting-rules': true,
-        'custom-properties': true,
-      },
+    plugins: {
+        'postcss-preset-env': {
+            importFrom: 'src/defaults.css',
+            features: {
+                'nesting-rules': true,
+                'custom-properties': {
+                    preserve: false
+                },
+            },
+        },
     },
-  },
-}
+};
