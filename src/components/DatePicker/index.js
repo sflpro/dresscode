f433560d7f8c1e9f575f1bb2171280ea9e58ee7d
aannnaa7 <anna.minasyan@sflpro.com>
2019-03-21 12:38:40 +0400

    DatePicker component implementation

--- src/components/DatePicker/index.js
+++ src/components/DatePicker/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import DayPicker, { LocaleUtils } from 'react-day-picker';
 
+import { Icon } from '../Icon';
 import { isMobile } from '../../utils';
 
 import 'style-loader!css-loader?modules=false!react-day-picker/lib/style.css';
@@ -12,78 +13,210 @@ const VIEW_TYPES = {
   DAY: 'day',
   MONTH: 'month',
 };
+const MONTHS_SHORT = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const date = new Date();
-const currentYear = date.getFullYear();
-const currentMonth = date.getMonth();
-
-const DatePickerCaption = ({ selectedDay, onMonthYearChange, view = VIEW_TYPES.DAY }) => {
-  const selectedDate = new Date(selectedDay);
+const formatMonthTitle = ({
+  selectedDay,
+  locale = 'en',
+  localeUtils,
+  months = MONTHS,
+}) => {
+  if (localeUtils) {
+    return localeUtils.formatMonthTitle(selectedDay, locale);
+  }
 
   return (
-    <div
-      className={styles.caption}
-      onClick={onMonthYearChange}
-    >
-      {view === VIEW_TYPES.DAY && (
-        LocaleUtils.formatMonthTitle(selectedDate)
-      )}
-      {view === VIEW_TYPES.MONTH && (
-        selectedDate.getFullYear()
-      )}
+    `${months[selectedDay.getMonth()]} ${selectedDay.getFullYear()}`
+  );
+};
+
+const DatePickerCaption = ({
+  selectedDay = new Date(),
+  onViewChange = null,
+  view = VIEW_TYPES.DAY,
+  localeUtils,
+  locale,
+  months,
+}) => {
+  const captionWrapperClasses = classNames({
+    [styles.captionWrapper]: true,
+    [styles[view]]: true,
+  });
+
+  return (
+    <div className={styles.caption}>
+      <div
+        className={captionWrapperClasses}
+        onClick={onViewChange}
+      >
+        {view === VIEW_TYPES.DAY && (
+          formatMonthTitle({ selectedDay, locale, localeUtils, months })
+        )}
+        {view === VIEW_TYPES.MONTH && (
+          selectedDay.getFullYear()
+        )}
+      </div>
     </div>
   );
-}
+};
 
-const DatePickerNavBar = () => {
+const DatePickerNavBar = ({
+  onPreviousClick,
+  onNextClick,
+}) => {
+  const leftIconStyles = classNames({
+    [styles.navbarIcon]: true,
+    [styles.left]: true,
+  });
+  const rightIconStyles = classNames({
+    [styles.navbarIcon]: true,
+    [styles.right]: true,
+  });
   return (
     <div
       className={styles.navbar}
     >
-
+      <Icon
+        name='arrow-left'
+        size={24}
+        className={leftIconStyles}
+        onClick={() => onPreviousClick()}
+      />
+      <Icon
+        name='arrow-right'
+        size={24}
+        className={rightIconStyles}
+        onClick={() => onNextClick()}
+      />
     </div>
   );
-}
+};
 
-const MonthPicker = ({ captionElement }) => {
-  // const fromMonth = new Date(currentYear, 0);
-  // const toMonth = new Date(currentYear + 10, 11);
-  console.log(LocaleUtils.getMonths());
+const MonthPicker = ({
+  selectedDay,
+  captionElement,
+  navbarElement,
+  monthsShort,
+  onMonthClick,
+  style,
+  ...props
+}) => {
+  const selectedMonth = selectedDay.getMonth();
+
   return (
-    <div>
-      {captionElement}
-      <div>
-        dfsdfdsf
+    <div
+      className={styles.MonthPicker}
+      style={style}
+      {...props}
+    >
+      {navbarElement}
+      <div className={styles.MonthPickerMonths}>
+        {captionElement}
+        <div className={styles.MonthPickerBody}>
+          {monthsShort.map((month, monthIndex) => {
+            const monthClasses = classNames({
+              [styles.MonthPickerMonth]: true,
+              [styles.active]: monthIndex === selectedMonth,
+            });
+
+            return (
+              <div
+                className={monthClasses}
+                onClick={() => onMonthClick(monthIndex, selectedDay)}
+                key={monthIndex}
+              >
+                {month}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export class DatePicker extends React.Component {
-  state = {
-    month: currentMonth,
-    year: currentYear,
-    view: VIEW_TYPES.DAY,
+  constructor(props) {
+    super(props);
+
+    const { view = VIEW_TYPES.DAY, value } = this.props;
+
+    this.state = {
+      view,
+      year: (value || new Date()).getFullYear(),
+    };
   }
 
-  handleViewChange = (view) => () => {
+  handleViewChange = (view) => {
     this.setState({
       view,
     });
   };
 
-  handleDayChange = () => {
+  handleDayClick = (day) => {
+    const { onChange } = this.props;
+    onChange(day);
+  };
 
-  }
+  handleMonthClick = (month, selectedDay) => {
+    const { view } = this.props;
+    const day = new Date(selectedDay).setMonth(month);
+    this.handleDayClick(new Date(day));
+    if (view !== VIEW_TYPES.MONTH) {
+      this.handleViewChange(VIEW_TYPES.DAY);
+    }
+  };
+
+  handlePreviousYearClick = (year) => {
+    this.setState({
+      year: year - 1,
+    });
+  };
+
+  handleNextYearClick = (year) => {
+    this.setState({
+      year: year + 1,
+    });
+  };
 
   render() {
-    const { value = '', className, style, ...props } = this.props;
-    const { month, year, view } = this.state;
-
+    const {
+      value,
+      locale = 'en',
+      monthsShort = MONTHS_SHORT,
+      months = MONTHS,
+      className = '',
+      style,
+      onChange,
+      ...props
+    } = this.props;
+    const { localeUtils = null } = this.props;
+    const { view, year } = this.state;
     const isNativeMode = isMobile();
+
+    const selectedDay = value || new Date();
+
     const datePickerClasses = classNames({
       [styles.datePicker]: true,
+      [className]: true,
     });
+
+    const dayPickerDefaultClasses = DayPicker.defaultProps.classNames;
+    const dayPickerClasses = {
+      ...dayPickerDefaultClasses,
+      container: `${dayPickerDefaultClasses.container} ${styles.DatePicker}`,
+      wrapper: `${dayPickerDefaultClasses.wrapper} ${styles.DatePickerWrapper}`,
+      month: `${dayPickerDefaultClasses.month} ${styles.DatePickerMonth}`,
+      weekdays: `${dayPickerDefaultClasses.weekdays} ${styles.DatePickerWeekdays}`,
+      weekday: `${dayPickerDefaultClasses.weekday} ${styles.DatePickerWeekday}`,
+      weekdaysRow: `${dayPickerDefaultClasses.weekdaysRow} ${styles.DatePickerWeekdaysRow}`,
+      week: `${dayPickerDefaultClasses.week} ${styles.DatePickerWeek}`,
+      day: `${dayPickerDefaultClasses.day} ${styles.DatePickerDay}`,
+      outside: `${styles.DatePickerDayOutside}`,
+      today: `${styles.DatePickerToday}`,
+      selected: `${styles.DatePickerSelectedDay}`,
+    };
 
     if (isNativeMode) {
       return null;
@@ -92,34 +225,55 @@ export class DatePicker extends React.Component {
     return (
       <div
         className={datePickerClasses}
-        style={style}
-        {...props}
       >
         {view === VIEW_TYPES.DAY && (
           <DayPicker
+            classNames={dayPickerClasses}
             captionElement={({ date }) => (
               <DatePickerCaption
                 selectedDay={date}
-                onViewChange={this.handleViewChange(VIEW_TYPES.MONTH)}
+                onViewChange={() => this.handleViewChange(VIEW_TYPES.MONTH)}
+                view={view}
+                locale={locale}
+                months={months}
+                localeUtils={localeUtils}
               />
             )}
             navbarElement={
-              <DatePickerNavBar
-              />
+              <DatePickerNavBar />
             }
-            onDayChange={this.handleDayChange}
-            selectedDay={value}
+            onDayClick={this.handleDayClick}
+            selectedDays={selectedDay}
+            month={selectedDay}
+            showOutsideDays
+            locale={locale}
+            style={style}
+            months={months}
+            {...props}
           />
         )}
         {view === VIEW_TYPES.MONTH && (
           <MonthPicker
-            selectedDay={value}
+            selectedDay={selectedDay}
             captionElement={
               <DatePickerCaption
-                year={year}
-                onViewChange={this.handleViewChange(VIEW_TYPES.DAY)}
+                selectedDay={new Date(selectedDay.setFullYear(year))}
+                view={view}
+                locale={locale}
+                months={months}
+                localeUtils={localeUtils}
               />
             }
+            navbarElement={
+              <DatePickerNavBar
+                onPreviousClick={() => this.handlePreviousYearClick(year)}
+                onNextClick={() => this.handleNextYearClick(year)}
+              />
+            }
+            onMonthClick={this.handleMonthClick}
+            monthsShort={monthsShort}
+            style={style}
+            {...props}
           />
         )}
       </div>
@@ -128,5 +282,20 @@ export class DatePicker extends React.Component {
 }
 
 DatePicker.propTypes = {
-  value: PropTypes.string,
+  value: PropTypes.instanceOf(Date),
+  locale: PropTypes.string,
+  localeUtils: PropTypes.shape({
+    formatDay: PropTypes.func.isRequired,
+    formatMonthTitle: PropTypes.func.isRequired,
+    formatWeekdayLong: PropTypes.func.isRequired,
+    formatWeekdayShort: PropTypes.func.isRequired,
+    getFirstDayOfWeek: PropTypes.func.isRequired,
+  }),
+  months: PropTypes.array,
+  weekdaysLong: PropTypes.array,
+  weekdaysShort: PropTypes.array,
+  firstDayOfWeek: PropTypes.number,
+  view: PropTypes.oneOf(Object.values(VIEW_TYPES)),
+  monthsShort: PropTypes.array,
+  onChange: PropTypes.func,
 };
