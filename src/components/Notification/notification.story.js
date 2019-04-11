aa2678b24193cd8103eb4a741b28d942fef95b39
Gagik Arustamyan <g.arustamyan@gmail.com>
2019-04-11 17:07:28 +0400

    Fix the notification display problem in the story

--- src/components/Notification/notification.story.js
+++ src/components/Notification/notification.story.js
@@ -1,89 +1,62 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import {State, Store} from "@sambego/storybook-state";
+import { State, Store } from '@sambego/storybook-state';
 
 import { ItemGroup } from '../../helpers/ItemGroup';
 import { Item } from '../../helpers/Item';
 
-import { Info, Warning, Error } from './Test';
-import {NotificationFrame} from "../NotificationFrame";
-import {Notification} from "./index";
-import {Button} from "../Button";
+import { NotificationFrame } from '../NotificationFrame';
+import { Notification } from './index';
+import { Button } from '../Button';
 
 storiesOf('Notification', module)
   .add('Notifications', () => {
     const store = new Store({
-      notification: [],
+      notification: null,
     });
 
     const showNotification = (notificationType) => {
-      const notification = Array.from(new Set([...store.state.notification, notificationType]));
       store.set({
-        ...store.state,
-        notification: notification,
+        notification: notificationType,
       });
     };
 
     return (
-        <State
-            store={store}
-        >
-          {state => (
-            <ItemGroup
-                title='Large Notifications'
-            >
-              {state.notification.includes('information') && (
+      <State store={store}>
+        {state => (
+          <ItemGroup title='Large Notifications'>
+            {state.notification
+              && (
                 <Notification>
                   <NotificationFrame
-                      status='information'
-                      message='Information Notification'
-                      title='Information'
-                    />
-                </Notification>
-              )}
-
-
-              {state.notification.includes('warning') && (
-                <Notification>
-                  <NotificationFrame
-                    status='warning'
-                    message='Warning Notification'
-                    title='Warning'
-                    dismiss
+                    status={state.notification}
+                    message={`${state.notification} notification`}
+                    title={state.notification}
                   />
                 </Notification>
-              )}
+              )
+            }
 
-              <Item>
-                <Button onClick={() => showNotification('information')}>
-                  Information
-                </Button>
-              </Item>
+            <Item>
+              <Button onClick={() => showNotification('information')}>
+                Information
+              </Button>
+            </Item>
 
+            <Item>
+              <Button onClick={() => showNotification('warning')}>
+                Warning
+              </Button>
+            </Item>
 
-              <Item>
-                <Button onClick={() => showNotification('warning')}>
-                  Information
-                </Button>
-              </Item>
-<br />
-<br />
-<br />
-<br />
-<br />
-              <Item>
-                <Info/>
-              </Item>
-              <br/>
-              <Item>
-                <Warning/>
-              </Item>
-              <br/>
-              <Item>
-                <Error/>
-              </Item>
-            </ItemGroup>
-          )}
-        </State>
-    )
+            <Item>
+              <Button onClick={() => showNotification('error')}>
+                Error
+              </Button>
+            </Item>
+
+          </ItemGroup>
+        )}
+      </State>
+    );
   });
