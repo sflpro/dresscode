1aff9d16feab610db1a76538796e84b2fa44132c
aannnaa7 <anna.minasyan@sflpro.com>
2019-05-10 17:42:53 +0400

    Pagination responsive view fixed, unstaged file added

---
+++ src/Table/constants.js
@@ -0,0 +1,429 @@
+export const SORTING_DIRECTIONS = {
+  ASC: 'asc',
+  DESC: 'desc',
+};
+
+export const COLUMNS = [{
+  id: 'firstName',
+  children: 'First Name',
+  priority: 1,
+  minWidth: 150,
+  sortable: true,
+},
+{
+  id: 'lastName',
+  children: 'Last Name',
+  priority: 3,
+  minWidth: 100,
+  sortable: true,
+},
+{
+  id: 'age',
+  children: 'Age',
+  priority: 2,
+  minWidth: 200,
+  sortable: true,
+}];
+
+export const ROWS = [
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'john',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'smith',
+      },
+      age: {
+        id: 'age',
+        children: '19',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'dfdfd',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'fhfhfgh',
+      },
+      age: {
+        id: 'age',
+        children: '34',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'fesrer',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'ghghgh',
+      },
+      age: {
+        id: 'age',
+        children: '24',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+  {
+    columns: {
+      firstName: {
+        id: 'firstName',
+        children: 'asery',
+      },
+      lastName: {
+        id: 'lastName',
+        children: 'deret',
+      },
+      age: {
+        id: 'age',
+        children: '56',
+      },
+    },
+  },
+];
