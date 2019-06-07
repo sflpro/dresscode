1b8adb6dbc2b119b865c80bab97fd7c9601f8be0
aannnaa7 <anna.minasyan@sflpro.com>
2019-06-07 12:44:34 +0400

    Date input validation fixed

--- src/DatePicker/helpers.js
+++ src/DatePicker/helpers.js
@@ -127,3 +127,155 @@ export const validateFormat = (dateString, format) => {
     date,
   };
 };
+
+export const isValidFormatType = format => (
+  DATE_FORMATS.includes(format)
+);
+
+export const isValidFormat = (dateString, format) => {
+  let seperator;
+  if (dateString.indexOf('/') > -1) {
+    seperator = '/';
+  } else if (dateString.indexOf('-') > -1) {
+    seperator = '-';
+  } else if (dateString.indexOf('.') > -1) {
+    seperator = '.';
+  }
+
+  if (!seperator || format.indexOf(seperator) === -1) {
+    return false;
+  }
+
+  const dateComponents = dateString.split(seperator);
+  const formatComponents = format.split(seperator);
+
+  if (dateComponents.length !== formatComponents.length) {
+    return false;
+  }
+
+  return true;
+};
+
+export const isValidDate = (dateString, format) => {
+  let seperator;
+  if (dateString.indexOf('/') > -1) {
+    seperator = '/';
+  } else if (dateString.indexOf('-') > -1) {
+    seperator = '-';
+  } else if (dateString.indexOf('.') > -1) {
+    seperator = '.';
+  }
+
+  if (!seperator || format.indexOf(seperator) === -1) {
+    return false;
+  }
+
+  const dateComponents = dateString.split(seperator);
+  const formatComponents = format.split(seperator);
+
+  if (dateComponents.length !== formatComponents.length) {
+    return false;
+  }
+
+  let valid = true;
+  for (let i = 0; i < formatComponents.length; i++) {
+    const formatComponent = formatComponents[i];
+    const dateComponent = dateComponents[i];
+
+    switch (formatComponent) {
+      case 'DD': {
+        valid = (dateComponent.length === 2 || (dateComponent.length === 1 && Number(dateComponent) < 9))
+          && Number(dateComponent) <= 31;
+        break;
+      }
+      case 'D': {
+        valid = (dateComponent.length === 2 || (dateComponent.length === 1 && Number(dateComponent) < 9))
+          && Number(dateComponent) <= 31;
+        break;
+      }
+      case 'MM': {
+        valid = (dateComponent.length === 2 || (dateComponent.length === 1 && Number(dateComponent) < 9))
+          && Number(dateComponent) >= 1 && Number(dateComponent) <= 12;
+        break;
+      }
+      case 'YY': {
+        valid = dateComponent.length === 2;
+        break;
+      }
+      case 'YYYY': {
+        valid = dateComponent.length === 4;
+        break;
+      }
+      default: {
+        valid = false;
+        break;
+      }
+    }
+
+    if (!valid) {
+      break;
+    }
+  }
+
+  return valid;
+};
+
+export const convertStringToDate = (dateString, format) => {
+  if (!dateString) {
+    return null;
+  }
+
+  let seperator;
+  if (dateString.indexOf('/') > -1) {
+    seperator = '/';
+  } else if (dateString.indexOf('-') > -1) {
+    seperator = '-';
+  } else if (dateString.indexOf('.') > -1) {
+    seperator = '.';
+  }
+
+  const dateComponents = dateString.split(seperator);
+  const formatComponents = format.split(seperator);
+
+  const date = {
+    d: null,
+    m: null,
+    y: null,
+  };
+  for (let i = 0; i < formatComponents.length; i++) {
+    const formatComponent = formatComponents[i];
+    const dateComponent = dateComponents[i];
+
+    switch (formatComponent) {
+      case 'DD': {
+        date.d = dateComponent;
+        break;
+      }
+      case 'D': {
+        date.d = dateComponent;
+        break;
+      }
+      case 'MM': {
+        date.m = dateComponent;
+        break;
+      }
+      case 'YY': {
+        date.y = dateComponent;
+        break;
+      }
+      case 'YYYY': {
+        date.y = dateComponent;
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+
+  if (!date.d) {
+    return new Date(`${date.y}-${date.m}-01`);
+  }
+
+  return new Date(`${date.y}-${date.m}-${date.d}`);
+};
