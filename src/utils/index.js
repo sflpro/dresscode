df807e026e88e5f5fcaea7babeba9b1860693658
Gevorg Sekhleyan <vnd_gevorg@beaxy.com>
2019-11-11 12:11:16 +0400

    fix: table crushing issue on resize or update case

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,18 @@
 export function isMobile() {
   return window.matchMedia('(max-width: 1024px)').matches;
 }
+
+export const getDebounce = (func, wait) => {
+  let timeout;
+
+  return function debounce(...args) {
+    const context = this;
+    const later = () => {
+      timeout = null;
+
+      func.apply(context, args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+};
