5924f7c96deb4072043df322992cacc4084c5c9d
Edo Poghosyan <edgar.poghosyan@sfl.am>
2020-07-14 14:13:16 +0400

    fix: control open state from outside of component

--- src/DropDown/index.js
+++ src/DropDown/index.js
@@ -7,6 +7,7 @@ import { Popover } from '../Popover';
 import { Toggle } from '../Toggle';
 
 export function DropDown({
+  changeState: propsChangeState,
   onStateChange,
   disabled,
   options,
@@ -14,6 +15,29 @@ export function DropDown({
   open,
   ...props
 }) {
+  if (propsChangeState) {
+    return (
+      <Popover
+        onTargetEvent={(nextIsOpen) => {
+          if (disabled) {
+            return;
+          }
+
+          propsChangeState(nextIsOpen);
+        }}
+        content={options}
+        closeOnScroll={false}
+        contentRelative
+        trigger='click'
+        open={open}
+        gap={8}
+        {...props}
+      >
+        {children}
+      </Popover>
+    );
+  }
+
   return (
     <Toggle defaultState={open}>
       {({ state: isOpen, changeState }) => (
@@ -55,6 +79,8 @@ DropDown.propTypes = {
   disabled: PropTypes.bool,
   /** Elements, content of dropdown tag */
   children: PropTypes.any.isRequired,
+  /** Function, called when dropdown state changed */
+  changeState: PropTypes.func,
 };
 
 DropDown.defaultProps = {
